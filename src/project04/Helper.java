39903388cd3c0e2c575bd49833f9647e299ee46f
Stephen <103603716+Salvarez99@users.noreply.github.com>
2022-04-16 20:22:54 -0400

    Update Helper.java

--- src/project04/Helper.java
+++ src/project04/Helper.java
@@ -6,12 +6,7 @@ public class Helper {
 	}
 
 	public static void create(){
-		
 
-		
-		
-		
-		
 		Course obj = new Course(123, "Science", 401, "Title", "Undergraduate");
 		Course obj2 = new Course(205, "Science", 401, "Title", "Undergraduate");
 		Course obj3 = new Course(100, "Science", 401, "Title", "Undergraduate");
@@ -22,6 +17,14 @@ public class Helper {
 		Course obj8 = new Course(220, "Science", 401, "Title", "Undergraduate");
 		
 		BinarySearchTree<Course> tree = new BinarySearchTree<Course>(obj);
+	
+		tree.insert(obj2);
+		tree.insert(obj3);
+		tree.insert(obj4);
+		tree.insert(obj5);
+		tree.insert(obj6);
+		tree.insert(obj7);
+		tree.insert(obj8);
 	
 		System.out.println(tree.size());
 		tree.makeEmpty();
